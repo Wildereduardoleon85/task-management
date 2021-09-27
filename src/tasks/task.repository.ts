8e1088277ee5454248786f4d2c8997f7e8179359
Wilder León <wilder_eduardo85@hotmail.com>
2021-09-27 20:22:26 -0300

    fixed connection to database issue

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,5 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { Task } from './task.entity';
 
-@EntityRepository()
+@EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {}
