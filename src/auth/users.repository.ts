e84dab8812283369f9b280e172ace765b20a9583
Wilder León <wilder_eduardo85@hotmail.com>
2021-09-29 12:34:20 -0300

    manage error handling for created an existing user

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository, EntityRepository } from 'typeorm';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import { User } from './user.entity';
@@ -8,6 +12,15 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username alredy exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
