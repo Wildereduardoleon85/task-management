5a5d5c48225fe11cd78efa00bf92982afed6da38
Wilder León <wilder_eduardo85@hotmail.com>
2021-09-29 11:40:57 -0300

    added auth module, service, controller, entity and repository

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersRepository } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
+})
+export class AuthModule {}
